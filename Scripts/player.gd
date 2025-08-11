ecb92373b7a01b2a06547cdd6e7105386914e1fd
Wol514 <2944969299@qq.com>
2025-08-11 19:42:45 +0800

    bug修复，墙体增加

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 20
+@export var animator : AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.	
@@ -8,6 +9,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	velocity = Input.get_vector("left","right","up","down") * move_speed
-	print(velocity)
+	if velocity == Vector2.ZERO : 
+		animator.play("Idle")
+		#print("Idle")
+	else :
+		animator.play("Run")
+		#print("Run")
 	move_and_slide()
 	pass
